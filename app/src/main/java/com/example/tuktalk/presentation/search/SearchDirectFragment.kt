ff8d0095576cf7b8c2b0c4b83f1aace82e303b9b
hanchang97 <nimok97@gmail.com>
2021-11-04 04:52:31 +0900

    refactor : 직접검색 & 키보드 처리 수정

--- app/src/main/java/com/example/tuktalk/presentation/search/SearchDirectFragment.kt
+++ app/src/main/java/com/example/tuktalk/presentation/search/SearchDirectFragment.kt
@@ -2,11 +2,16 @@ package com.example.tuktalk.presentation.search
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.tuktalk.R
@@ -51,6 +56,47 @@ class SearchDirectFragment: Fragment() {
         binding.ivBack.setOnClickListener {
             (parentFragment as SearchFragment).goToSearchSelect()
         }
+
+        //////////////////////////////////////   * edit text 의 inputType 'text'로 해주면 완료버튼 보임
+        // 직접 검색 후 키보드에서 완료버튼 누르면 닫힘
+        binding.etDirectSearch.setOnEditorActionListener { textView, action, keyEvent ->
+            var handled = false
+            if(action == EditorInfo.IME_ACTION_DONE){
+                Log.e("AppTest", "키워드 입력 후 키보드 완료 버튼 누름")
+                val inputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(binding.etDirectSearch.windowToken, 0)
+
+                handled = true
+            }
+
+            handled
+        }
+
+        binding.etDirectSearch.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // 길이 0 되면 첫 화면 보임
+                var keywordText = binding.etDirectSearch.text
+                if(keywordText.length <= 0){
+                    binding.clDirectRV.visibility = View.INVISIBLE
+                    binding.clDirectDes.visibility = View.VISIBLE
+                }
+                else{
+                    // 리사이클러뷰 초기화 먼저 해주기!!
+                    //
+                    binding.clDirectRV.visibility = View.VISIBLE
+                    binding.clDirectDes.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
 
 
     }
---
+++ app/src/main/java/com/example/tuktalk/presentation/search/adpater/SearchITRVadapter.kt
@@ -0,0 +1,114 @@
+package com.example.tuktalk.presentation.search.adpater
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tuktalk.R
+import com.example.tuktalk.databinding.ItemSearchDesignRecycler1Binding
+import com.example.tuktalk.databinding.ItemSearchDesignRecycler2Binding
+import com.example.tuktalk.databinding.ItemSearchDesignRecycler3LoadingBinding
+import com.example.tuktalk.domain.model.search.PortfolioRV_item
+import okhttp3.Challenge
+import java.lang.RuntimeException
+
+class SearchITRVadapter(
+        private var dataSet : MutableList<PortfolioRV_item>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    //var dataSet = mutableListOf<PortfolioRV_item>()
+
+    // 뷰 타입 오버라이드! 1. 설명 텍스트뷰 2. 포트폴리오 리스트 아이템 3. 로딩 뷰
+    override fun getItemViewType(position: Int): Int {
+        return dataSet[position].itemViewType
+    }
+
+    // 상단 텍스트 뷰
+    inner class ViewType1ViewHolder(val binding: ItemSearchDesignRecycler1Binding)
+        :RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    // 포트폴리오 아이템 뷰
+    inner class ViewType2ViewHolder(val binding: ItemSearchDesignRecycler2Binding)
+        :RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    // 다음 페이지 정보 요청 시 로딩 프로그레스바 뷰
+    inner class ViewType3ViewHolder(val binding: ItemSearchDesignRecycler3LoadingBinding)
+        :RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return when(viewType){
+            1->{ // 상단 텍스트 1개를 위한 뷰
+                val view = LayoutInflater.from(parent.context).inflate(
+                        R.layout.item_search_design_recycler_1,
+                        parent,
+                        false
+                )
+
+                // 비율로 높이 설정하기
+                var params = view.layoutParams
+                params.height = parent.measuredHeight / 527 * 80
+
+                ViewType1ViewHolder(ItemSearchDesignRecycler1Binding.bind(view))
+            }
+            2->{ // 실제 포트폴리오 아이템 뷰
+                val view = LayoutInflater.from(parent.context).inflate(
+                        R.layout.item_search_design_recycler_2,
+                        parent,
+                        false
+                )
+
+                // 비율로 높이 설정하기  // recyclerview 영역이 0dp + left,right constraint = parent 로 해줘야 인식되는 듯
+                var params = view.layoutParams
+                params.height = parent.measuredHeight / 527 * 180
+
+                ViewType2ViewHolder(ItemSearchDesignRecycler2Binding.bind(view))
+            }
+            3 -> { // 로딩 뷰
+                val view = LayoutInflater.from(parent.context).inflate(
+                        R.layout.item_search_design_recycler_3_loading,
+                        parent,
+                        false
+                )
+
+                ViewType3ViewHolder(ItemSearchDesignRecycler3LoadingBinding.bind(view))
+            }
+            else -> {
+                throw RuntimeException("unknown item view type error")
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is ViewType1ViewHolder){
+
+        }
+        else if(holder is ViewType2ViewHolder){
+
+        }
+        else if(holder is ViewType3ViewHolder){
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+
+    fun updateList(newList: MutableList<PortfolioRV_item>){
+        dataSet = newList
+        notifyDataSetChanged()
+    }
+
+    fun clearList(){
+        dataSet.clear()
+    }
+
+}
