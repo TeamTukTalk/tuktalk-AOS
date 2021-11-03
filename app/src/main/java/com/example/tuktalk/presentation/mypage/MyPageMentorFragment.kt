d7948563067e85fc5d3531f89a2b3cad83406491
hanchang97 <nimok97@gmail.com>
2021-11-04 05:36:19 +0900

    feat : 마이페이지 옵션 메뉴 생성

--- app/src/main/java/com/example/tuktalk/presentation/mypage/MyPageMentorFragment.kt
+++ app/src/main/java/com/example/tuktalk/presentation/mypage/MyPageMentorFragment.kt
@@ -3,9 +3,8 @@ package com.example.tuktalk.presentation.mypage
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.tuktalk.R
@@ -23,6 +22,8 @@ class MyPageMentorFragment: Fragment() {
         Log.e("AppTest", "mentor fragment onCreateView")
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mypage_mentor, container, false)
+
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -31,24 +32,38 @@ class MyPageMentorFragment: Fragment() {
         Log.e("AppTest", "mentor fragment onViewCreated")
 
         // 모드 전환 액티비티로 이동
-        binding.btnMentor.setOnClickListener {
+       /* binding.btnMentor.setOnClickListener {
             Log.e("AppTest","go to test2 activity")
             val intent = Intent(context, Test2Activity::class.java)
             startActivity(intent)
-        }
+        }*/
 
         // 멘토 등록 액티비티 이동
-        binding.btnMentorRegist.setOnClickListener {
+       /* binding.btnMentorRegist.setOnClickListener {
             Log.e("AppTest","go to mentor regist activity")
             val intent = Intent(context, MentorRegistActivity::class.java)
             startActivity(intent)
-        }
+        }*/
 
+        // toolbar 설정!!
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)  // 액션바로 xml에 만들어준 toolbar를 사용한다
+        (activity as AppCompatActivity).supportActionBar!!.setDisplayShowTitleEnabled(false) // 기본제목 없애기
     }
 
     override fun onResume() {
         super.onResume()
 
         Log.e("AppTest", "mentor fragment onResume")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu2, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_option) {
+            Log.e("AppTest","fragment actionbar option icon clicked")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
