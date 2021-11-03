1f46aeb5d1f781d2447ffb1a511ac93d0ec2d4c4
hanchang97 <nimok97@gmail.com>
2021-11-04 00:41:36 +0900

    refactor : 태그bottom sheet 높이 조정 방법 및 뷰 비율 수정

--- app/src/main/java/com/example/tuktalk/presentation/search/dialog/TagDialogFragment.kt
+++ app/src/main/java/com/example/tuktalk/presentation/search/dialog/TagDialogFragment.kt
@@ -1,13 +1,21 @@
 package com.example.tuktalk.presentation.search.dialog
 
-import android.content.Context
-import android.graphics.Point
+import android.app.Activity
+import android.app.Dialog
 import android.os.Bundle
-import android.view.*
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import com.example.tuktalk.R
-import com.example.tuktalk.databinding.DialogRegistinfoBreakawayBinding
 import com.example.tuktalk.databinding.DialogSearchTagBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.lang.reflect.TypeVariable
+
 
 class TagDialogFragment : BottomSheetDialogFragment() {
 
@@ -17,22 +25,58 @@ class TagDialogFragment : BottomSheetDialogFragment() {
         super.onCreate(savedInstanceState)
 
         setStyle(STYLE_NORMAL, R.style.AppBottomSheetDialogTheme)
+
     }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setOnShowListener { dialogInterface ->
+            val bottomSheetDialog = dialogInterface as BottomSheetDialog
+            setupRatio(bottomSheetDialog)
+        }
+        return dialog
+    }
+
+    private fun setupRatio(bottomSheetDialog: BottomSheetDialog) {
+        //id = com.google.android.material.R.id.design_bottom_sheet for Material Components
+        //id = android.support.design.R.id.design_bottom_sheet for support librares
+
+        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
+        val behavior = BottomSheetBehavior.from(bottomSheet)
+        val layoutParams = bottomSheet!!.layoutParams
+        layoutParams.height = getBottomSheetDialogDefaultHeight()
+        bottomSheet.layoutParams = layoutParams
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getWindowHeight() * 548 / 760
+    }
+
+    private fun getWindowHeight(): Int {
+        // Calculate window height for fullscreen use
+        val displayMetrics = DisplayMetrics()
+        (context as Activity?)!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
+    }
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
     : View? {
-        binding = DialogSearchTagBinding.inflate(inflater, container, false)
+        val view = inflater.inflate(R.layout.dialog_search_tag, container, false)
+        binding = DialogSearchTagBinding.bind(view)
 
+        /*view.findViewById<ConstraintLayout>(R.id.cl_dialog).maxHeight =
+                (resources.displayMetrics.heightPixels * 0.5).toInt()*/
 
-        return binding.root
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
-        //다이얼로그 크기 설정
-        // 스크린 크기
-        var wm : WindowManager = getContext()?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        /*var wm : WindowManager = getContext()?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         var screen : Display = wm.getDefaultDisplay()
 
         var size = Point()
@@ -40,6 +84,19 @@ class TagDialogFragment : BottomSheetDialogFragment() {
         var height = size.y
         var width = size.x
 
-        binding.clDialog.layoutParams.height = width * 548 / 360
+        Log.e("AppTest", "search dialog onActivityCreated, screen height : ${height},  width : ${width}")
+
+        binding.llDialog.layoutParams.height = height / 4 * 3 */
+
+        //binding.clDialog.layoutParams.height = width * 560 / 300
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+
     }
 }
