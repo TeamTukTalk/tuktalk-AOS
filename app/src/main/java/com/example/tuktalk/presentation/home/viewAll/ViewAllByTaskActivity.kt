291961d5998f590bf0995a14770274b3a74a9db9
hanchang97 <nimok97@gmail.com>
2021-11-18 00:25:16 +0900

    refactor : 직무별 뚝딱멘토 전체보기 뷰 수정

--- app/src/main/java/com/example/tuktalk/presentation/home/viewAll/ViewAllByTaskActivity.kt
+++ app/src/main/java/com/example/tuktalk/presentation/home/viewAll/ViewAllByTaskActivity.kt
@@ -1,14 +1,18 @@
 package com.example.tuktalk.presentation.home.viewAll
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tuktalk.R
+import com.example.tuktalk.common.utils.VerticalItemDecorator
 import com.example.tuktalk.databinding.ActivityLoginBinding
 import com.example.tuktalk.databinding.ActivityViewallBytaskBinding
 import com.example.tuktalk.domain.model.home.HomeTop5MentorRVitem
 import com.example.tuktalk.domain.model.home.MentorListRVitem
+import com.example.tuktalk.domain.model.search.PortfolioRV_item
 import com.example.tuktalk.presentation.home.adapter.Top5MentorRVAdpater
 import com.example.tuktalk.presentation.home.viewAll.adapter.ViewAllByTaskRVAdapter
 
@@ -33,7 +37,48 @@ class ViewAllByTaskActivity: AppCompatActivity() {
         ///////////
 
         // 리사이클러뷰 설정
+        rvAdapter = ViewAllByTaskRVAdapter(testDataList)
+        binding.RVByTask.layoutManager = LinearLayoutManager(this)
+        binding.RVByTask.adapter = rvAdapter
+        binding.RVByTask.addItemDecoration(VerticalItemDecorator(15))
 
+        // 처음 상태는 디자인 토글 선택 -> 액티비티 생성되면서 디자인 값으로 리스트 가져오기!! @@@@@@@@@@@@@@@
+
+        testDataList.apply{
+            add(MentorListRVitem(0, "", "", "","",
+                    1))
+            add(MentorListRVitem(1, "제이슨", "네이버", "UIUX 디자인",
+                    "#이직 #실무 #상담 #UX #면접 #공채 #앱 #이직",
+                    2))
+            add(MentorListRVitem(1, "제이슨", "네이버", "UIUX 디자인","#이직 #실무 #상담 #UX #면접 #공채 #앱 #이직",
+                    2))
+            add(MentorListRVitem(1, "제이슨", "네이버", "UIUX 디자인",
+                    "#이직 #실무 #상담 #UX #면접 #공채 #앱 #이직",
+                    2))
+            add(MentorListRVitem(1, "제이슨", "네이버", "UIUX 디자인",
+                    "#이직 #실무 #상담 #UX #면접 #공채 #앱 #이직",
+                    2))
+        }
+
+        rvAdapter.updateList(testDataList)
+        /////////////////////////////////////////////////////////////////
+
+        // 직무별 cardview 토글
+        binding.cvByTaskDesign.setOnClickListener {
+            // 토글 선택 시 RV 리스트 초기화 후 통신 결과 리스트로 업데이트 해주기
+            // 이때 첫 번째는  empty item view 넣어주기!!
+            binding.cvByTaskDesign.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.tuktalk_primary))
+            binding.tvCategoryDesign.setTextColor(resources.getColor(R.color.white))
+            binding.cvByTaskIt.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.tuktalk_gray_5))
+            binding.tvCategoryIt.setTextColor(resources.getColor(R.color.tuktalk_sub_content_2))
+
+        }
+        binding.cvByTaskIt.setOnClickListener {
+            binding.cvByTaskIt.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.tuktalk_primary))
+            binding.tvCategoryIt.setTextColor(resources.getColor(R.color.white))
+            binding.cvByTaskDesign.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.tuktalk_gray_5))
+            binding.tvCategoryDesign.setTextColor(resources.getColor(R.color.tuktalk_sub_content_2))
+        }
 
     }
 
--- app/src/main/java/com/example/tuktalk/presentation/home/viewAll/adapter/ViewAllByTaskRVAdapter.kt
+++ app/src/main/java/com/example/tuktalk/presentation/home/viewAll/adapter/ViewAllByTaskRVAdapter.kt
@@ -4,10 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tuktalk.R
-import com.example.tuktalk.databinding.ItemEmptyViewBinding
-import com.example.tuktalk.databinding.ItemSearchDesignRecycler1Binding
-import com.example.tuktalk.databinding.ItemSearchDesignRecycler2Binding
-import com.example.tuktalk.databinding.ItemSearchDesignRecycler3LoadingBinding
+import com.example.tuktalk.databinding.*
 import com.example.tuktalk.domain.model.home.HomeTop5MentorRVitem
 import com.example.tuktalk.domain.model.home.MentorListRVitem
 import com.example.tuktalk.domain.model.search.PortfolioRV_item
@@ -30,7 +27,7 @@ class ViewAllByTaskRVAdapter(
     }
 
     // 멘토리스트 아이템 뷰
-    inner class ViewType2ViewHolder(val binding: ItemSearchDesignRecycler2Binding)
+    inner class ViewType2ViewHolder(val binding: ItemMentorListRecycler1Binding)
         :RecyclerView.ViewHolder(binding.root){
 
     }
@@ -39,7 +36,7 @@ class ViewAllByTaskRVAdapter(
         return when(viewType){
             1->{ // 상단 텍스트 1개를 위한 뷰
                 val view = LayoutInflater.from(parent.context).inflate(
-                        R.layout.item_search_design_recycler_1,
+                        R.layout.item_empty_view,
                         parent,
                         false
                 )
@@ -52,7 +49,7 @@ class ViewAllByTaskRVAdapter(
             }
             2->{ // 실제 포트폴리오 아이템 뷰
                 val view = LayoutInflater.from(parent.context).inflate(
-                        R.layout.item_search_design_recycler_2,
+                        R.layout.item_mentor_list_recycler_1,
                         parent,
                         false
                 )
@@ -61,7 +58,7 @@ class ViewAllByTaskRVAdapter(
                 var params = view.layoutParams
                 params.height = parent.measuredWidth / 328 * 135
 
-                ViewType2ViewHolder(ItemSearchDesignRecycler2Binding.bind(view))
+                ViewType2ViewHolder(ItemMentorListRecycler1Binding.bind(view))
             }
 
             else -> {
@@ -71,10 +68,10 @@ class ViewAllByTaskRVAdapter(
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if(holder is SearchDesignRVadapter.ViewType1ViewHolder){
+        if(holder is ViewAllByTaskRVAdapter.ViewType1ViewHolder){
 
         }
-        else if(holder is SearchDesignRVadapter.ViewType2ViewHolder){
+        else if(holder is ViewAllByTaskRVAdapter.ViewType2ViewHolder){
             holder.binding.tvMentorName.text = dataSet[position].mentorName  // 멘토 이름
             holder.binding.tvConmpanyName.text = dataSet[position].companyName // 회사명
             holder.binding.tvTaskName.text = dataSet[position].task // 업무명
@@ -85,4 +82,15 @@ class ViewAllByTaskRVAdapter(
     override fun getItemCount(): Int {
         return dataSet.size
     }
+
+    fun updateList(newList: MutableList<MentorListRVitem>){
+        dataSet = newList
+        notifyDataSetChanged()
+    }
+
+    fun clearList(){
+        dataSet.clear()
+        notifyDataSetChanged()
+    }
+
 }
