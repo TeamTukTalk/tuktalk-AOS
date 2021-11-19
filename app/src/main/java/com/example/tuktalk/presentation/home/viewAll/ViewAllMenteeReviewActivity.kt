0d6ef5ab6f8d6ff3604bd720d9c777d5a8ec748e
hanchang97 <nimok97@gmail.com>
2021-11-19 18:28:25 +0900

    refactor : 실시간 후기 리사이클러뷰 리뷰 내용 더보기 시 아이템뷰 높이 조절 수정

--- app/src/main/java/com/example/tuktalk/presentation/home/viewAll/ViewAllMenteeReviewActivity.kt
+++ app/src/main/java/com/example/tuktalk/presentation/home/viewAll/ViewAllMenteeReviewActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tuktalk.R
+import com.example.tuktalk.common.utils.VerticalItemDecorator
 import com.example.tuktalk.databinding.ActivityViewallBytaskBinding
 import com.example.tuktalk.databinding.ActivityViewallMenteeReviewBinding
+import com.example.tuktalk.domain.model.home.MentorListRVitem
+import com.example.tuktalk.domain.model.home.RealTimeMenteeReviewRVitem
+import com.example.tuktalk.domain.model.home.RealTimeMenteeReviewRVitem_viewAll
+import com.example.tuktalk.presentation.home.viewAll.adapter.ViewAllByTaskRVAdapter
+import com.example.tuktalk.presentation.home.viewAll.adapter.ViewAllMenteeReviewRVAdapter
 
 class ViewAllMenteeReviewActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityViewallMenteeReviewBinding
+
+    lateinit var rvAdapter: ViewAllMenteeReviewRVAdapter
+    private var testDataList = mutableListOf<RealTimeMenteeReviewRVitem_viewAll>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +33,41 @@ class ViewAllMenteeReviewActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼 활성화
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_icon_back)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
+        //////////////////////////////////////////////////////
+
+        rvAdapter = ViewAllMenteeReviewRVAdapter(testDataList)
+        binding.RVRealtimeMenteeReview.layoutManager = LinearLayoutManager(this)
+        binding.RVRealtimeMenteeReview.adapter = rvAdapter
+        binding.RVRealtimeMenteeReview.addItemDecoration(VerticalItemDecorator(15))
+
+        testDataList.apply{ // 임시 데이터
+            add(RealTimeMenteeReviewRVitem_viewAll(1, "", "", "",
+                    0,"","",
+                    " ", 1, false
+            ))
+            add(RealTimeMenteeReviewRVitem_viewAll(1, "리즈", "네이버", "UIUX 디자인",
+                    5,"애니","2021. 10. 12",
+                    "궁금했던 내용을 실무 경험으로 얘기해주셔서 너무 도움이 되었습니다. 포트폴리오를 만들려고 하니까 막막했는데 한 멘토님이 어떤 방법으로 제작해야 할지 방향을 잡아주셔서 좋았습니다.",
+                    2, false
+            ))
+            add(RealTimeMenteeReviewRVitem_viewAll(1, "제임스", "쿠팡", "앱",
+                    4,"에스","2021. 10. 12", "리뷰 테스트 한 줄 넘어가는 테스트",2, false
+            ))
+            add(RealTimeMenteeReviewRVitem_viewAll(1, "킴", "네이버", "데이터",
+                    1,"제이슨","2021. 10. 12",
+                    "리뷰 테스트 리뷰 테스트 긴 문장 테스트 입니다 뚝딱뚝딱뚝딱뚝딱뚝딱 뚝딱뚝딱뚝딱",2, false
+            ))
+            add(RealTimeMenteeReviewRVitem_viewAll(1, "브라이언", "삼성전자", "UIUX 디자인",
+                    2,"존","2021. 10. 12", "리뷰 테스트",2, false
+            ))
+        }
+        rvAdapter.updateList(testDataList)
+        /////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
     }
 
 
--- app/src/main/java/com/example/tuktalk/presentation/home/viewAll/adapter/ViewAllMenteeReviewRVAdapter.kt
+++ app/src/main/java/com/example/tuktalk/presentation/home/viewAll/adapter/ViewAllMenteeReviewRVAdapter.kt
@@ -1,4 +1,126 @@
 package com.example.tuktalk.presentation.home.viewAll.adapter
 
-class ViewAllMenteeReviewRVAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tuktalk.R
+import com.example.tuktalk.databinding.ItemEmptyViewBinding
+import com.example.tuktalk.databinding.ItemMentorListRecycler1Binding
+import com.example.tuktalk.databinding.ItemRealtimeMenteeReviewRecycler1Binding
+import com.example.tuktalk.domain.model.home.MentorListRVitem
+import com.example.tuktalk.domain.model.home.RealTimeMenteeReviewRVitem
+import com.example.tuktalk.domain.model.home.RealTimeMenteeReviewRVitem_viewAll
+import java.lang.RuntimeException
+
+class ViewAllMenteeReviewRVAdapter(
+        private var dataSet : MutableList<RealTimeMenteeReviewRVitem_viewAll>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    // 뷰 타입 오버라이드! 1. 빈 뷰 2. 포트폴리오 리스트 아이템 3. 로딩 뷰
+    override fun getItemViewType(position: Int): Int {
+        return dataSet[position].itemViewType
+    }
+
+    // 상단 empty 뷰
+    inner class ViewType1ViewHolder(val binding: ItemEmptyViewBinding)
+        : RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    // 멘토리스트 아이템 뷰
+    inner class ViewType2ViewHolder(val binding: ItemRealtimeMenteeReviewRecycler1Binding)
+        : RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType){
+            1->{ // 상단 텍스트 1개를 위한 뷰
+                val view = LayoutInflater.from(parent.context).inflate(
+                        R.layout.item_empty_view,
+                        parent,
+                        false
+                )
+
+                // 비율로 높이 설정하기
+                var params = view.layoutParams
+                params.height = parent.measuredWidth / 328 * 16
+
+                ViewType1ViewHolder(ItemEmptyViewBinding.bind(view))
+            }
+            2->{ // 실제 포트폴리오 아이템 뷰
+                val view = LayoutInflater.from(parent.context).inflate(
+                        R.layout.item_realtime_mentee_review_recycler_1,
+                        parent,
+                        false
+                )
+
+                // 비율로 높이 설정하기  // recyclerview 영역이 0dp + left,right constraint = parent 로 해줘야 인식되는 듯
+                /*var params = view.layoutParams
+                params.height = parent.measuredWidth / 328 * 135 */
+
+                ViewType2ViewHolder(ItemRealtimeMenteeReviewRecycler1Binding.bind(view))
+            }
+
+            else -> {
+                throw RuntimeException("unknown item view type error")
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is ViewAllMenteeReviewRVAdapter.ViewType1ViewHolder){
+
+        }
+        else if(holder is ViewAllMenteeReviewRVAdapter.ViewType2ViewHolder){
+            holder.binding.tvMentorName.text = dataSet[position].mentorName  // 멘토 이름
+            holder.binding.tvCompanyName.text = dataSet[position].companyName // 회사명
+            holder.binding.tvTaskName.text = dataSet[position].taskName // 업무명
+            holder.binding.tvMenteeName.text = dataSet[position].menteeName
+            holder.binding.tvReviewDate.text = dataSet[position].reviewDate
+            holder.binding.tvReviewContent2.text = dataSet[position].reviewContent.replace(" ", "\u00A0") // 리뷰 내용
+
+            //별점
+            when(dataSet[position].star){
+                0-> holder.binding.ivStar.setImageResource(R.drawable.ic_star_0)
+                1-> holder.binding.ivStar.setImageResource(R.drawable.ic_star_1)
+                2-> holder.binding.ivStar.setImageResource(R.drawable.ic_star_2)
+                3-> holder.binding.ivStar.setImageResource(R.drawable.ic_star_3)
+                4-> holder.binding.ivStar.setImageResource(R.drawable.ic_star_4)
+                5-> holder.binding.ivStar.setImageResource(R.drawable.ic_star_5)
+            }
+
+
+            // 더보기 클릭 시 리뷰 전체 내용 보여야 함
+            holder.binding.llViewallReview.setOnClickListener {
+                if(!dataSet[position].viewAllSelected){ // 더보기x 에서 더보기 누를 시
+                    holder.binding.tvReviewContent2.maxLines = 6  // 우선 더보기 누를 시 최대 6줄로 변경
+                    dataSet[position].viewAllSelected = true
+                    holder.binding.ivViewallReview.setImageResource(R.drawable.ic_up_arrow)
+                }
+                else{
+                    holder.binding.tvReviewContent2.maxLines = 3
+                    dataSet[position].viewAllSelected = false
+                    holder.binding.ivViewallReview.setImageResource(R.drawable.ic_down_arrow)
+                }
+
+            }
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    fun updateList(newList: MutableList<RealTimeMenteeReviewRVitem_viewAll>){
+        dataSet = newList
+        notifyDataSetChanged()
+    }
+
+    fun clearList(){
+        dataSet.clear()
+        notifyDataSetChanged()
+    }
+
 }
