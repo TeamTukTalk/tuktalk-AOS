cdfed60b58f706fd422c75706bc94227725afef1
hanchang97 <nimok97@gmail.com>
2021-11-23 01:36:18 +0900

    refactor : 로그인뷰 - edittext 외부 터치 시 키보드 내려가기 수정

--- app/src/main/java/com/example/tuktalk/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/tuktalk/presentation/login/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.example.tuktalk.presentation.login
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -14,7 +17,6 @@ import com.example.tuktalk.data.remote.dto.request.UserLoginRequestDto
 import com.example.tuktalk.databinding.ActivityLoginBinding
 import com.example.tuktalk.presentation.main.MainActivity
 import com.example.tuktalk.presentation.signup.SelectRoleActivity
-import com.example.tuktalk.presentation.signup.info.InfoRegistViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class LoginActivity: AppCompatActivity() {
@@ -35,49 +37,58 @@ class LoginActivity: AppCompatActivity() {
         // 로그인 activity 오른쪽에서 왼쪽으로 나타나는 애니메이션 적용
         overridePendingTransition(R.anim.slide_right_enter, R.anim.none)
 
-        // 아이디 입력 시 edittext 색상 변경
-       binding.etId.setOnFocusChangeListener(View.OnFocusChangeListener { view, focused ->
-            if(focused){
-                Log.e("AppTest","et id focused")
-                binding.etId.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.tuktalk_primary)
-                //binding.etId.error = "error test"
-            }
-            else{
-                Log.e("AppTest","et id focus x")
-                binding.etId.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.tuktalk_gray1)
+        // edit text 외부 터치 시 키보드 내려가게 하기
+        binding.clAll.setOnTouchListener(object: View.OnTouchListener{
+            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+                hideKeyboard()
+                return false
             }
         })
+
+
+        // 아이디 입력 시 edittext 색상 변경
+       binding.etId.setOnFocusChangeListener(View.OnFocusChangeListener { view, focused ->
+           if (focused) {
+               Log.e("AppTest", "et id focused")
+               binding.etId.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.tuktalk_primary)
+               //binding.etId.error = "error test"
+           } else {
+               Log.e("AppTest", "et id focus x")
+               binding.etId.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.tuktalk_gray1)
+           }
+       })
         //
 
         // 아이디 - 이메일 형식 체크
-        binding.etId.addTextChangedListener(object:TextWatcher{
+        binding.etId.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
+
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 var idText = binding.etId.text
                 var emailPattern = android.util.Patterns.EMAIL_ADDRESS
 
-                if(emailPattern.matcher(idText).matches()){
+                if (emailPattern.matcher(idText).matches()) {
                     binding.llErrorId.visibility = View.GONE
                     // 모두 0 값을 주면 아이콘 사라진다!!
-                    binding.etId.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, 0,0)
+                    binding.etId.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, 0, 0)
                     ID = idText.toString()
-                }
-                else{
+                } else {
                     binding.llErrorId.visibility = View.VISIBLE
 
                     // 동적으로 edittext에 아이콘 생성!!
-                    binding.etId.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_icon_error,0)
+                    binding.etId.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ic_icon_error, 0)
                 }
 
                 // 빈 칸 되었을 때
-                if(idText.length == 0){
+                if (idText.length == 0) {
                     binding.llErrorId.visibility = View.GONE
-                    binding.etId.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, 0,0)
+                    binding.etId.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, 0, 0)
                     //binding.etId.clearFocus()
                 }
             }
+
             override fun afterTextChanged(p0: Editable?) {
 
             }
@@ -87,13 +98,12 @@ class LoginActivity: AppCompatActivity() {
 
         // 비밀번호 입력 시 edittext 색상 변경
         binding.etPw.setOnFocusChangeListener(View.OnFocusChangeListener { view, focused ->
-            if(focused){
-                Log.e("AppTest","et pw focused")
+            if (focused) {
+                Log.e("AppTest", "et pw focused")
                 binding.etPw.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.tuktalk_primary)
                 //binding.etId.error = "error test"
-            }
-            else{
-                Log.e("AppTest","et pw focus x")
+            } else {
+                Log.e("AppTest", "et pw focus x")
                 binding.etPw.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.tuktalk_gray1)
             }
         })
@@ -104,15 +114,17 @@ class LoginActivity: AppCompatActivity() {
         //
         //
         //
-        binding.etPw.addTextChangedListener(object:TextWatcher{
+        binding.etPw.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
+
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 var pwText = binding.etPw.text
                 PASSWORD = pwText.toString()
 
             }
+
             override fun afterTextChanged(p0: Editable?) {
 
             }
@@ -128,17 +140,16 @@ class LoginActivity: AppCompatActivity() {
         }
 
         viewModel.isLoginSuccess.observe(this, {
-            if(it){
+            if (it) {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
-            }
-            else{
+            } else {
                 Toast.makeText(this, "로그인에 실패하였습니다", Toast.LENGTH_SHORT).show()
             }
         })
 
-        viewModel.progressBarVisibility.observe(this,{
-            if(it)
+        viewModel.progressBarVisibility.observe(this, {
+            if (it)
                 binding.loadingProgressBar.visibility = View.VISIBLE
             else
                 binding.loadingProgressBar.visibility = View.INVISIBLE
@@ -147,7 +158,7 @@ class LoginActivity: AppCompatActivity() {
 
         // 회원가입 클릭 시 -> 회원가입 화면으로 이동하기
         binding.tvSignUp.setOnClickListener {
-            Log.e("AppTest","go to select role activity")
+            Log.e("AppTest", "go to select role activity")
             val intent = Intent(this, SelectRoleActivity::class.java)
             startActivity(intent)
         }
@@ -161,5 +172,11 @@ class LoginActivity: AppCompatActivity() {
         binding.etPw.text = null
         binding.etId.clearFocus()
         binding.etPw.clearFocus()
+    }
+
+    // edit text 외부 터치 시 키보드 내려가게
+    fun hideKeyboard() {
+        val inputManager: InputMethodManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(this.currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
 }
--- app/src/main/java/com/example/tuktalk/presentation/mypage/mentor/mentorProfile/step/MentorProfileStep1Fragment.kt
+++ app/src/main/java/com/example/tuktalk/presentation/mypage/mentor/mentorProfile/step/MentorProfileStep1Fragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.tuktalk.R
@@ -30,6 +31,19 @@ class MentorProfileStep1Fragment: Fragment(){
         // 닉네임은 가져오기
         binding.etNickname.setText(Constants.USER_NICKNAME)  // edittext의 경우 text= 형태가 아닌 setText로 텍스트 값 할당!
 
+        // 한 줄 소개 edit text 활성화 시 커서, cardview 테두리 색상 변경하기
+        binding.etOneIntroduce.setOnFocusChangeListener(View.OnFocusChangeListener { view, focused ->
+            if(focused){
+                Log.e("AppTest","et one introduce focused")
+                binding.cvOneIntroduce.strokeColor = resources.getColor(R.color.tuktalk_primary)
+                binding.cvOneIntroduce.invalidate()
+            }
+            else{
+                Log.e("AppTest","et one introduce  focus x")
+                binding.cvOneIntroduce.strokeColor = resources.getColor(R.color.tuktalk_gray_1)
+                binding.cvOneIntroduce.invalidate()
+            }
+        })
 
 
     }
